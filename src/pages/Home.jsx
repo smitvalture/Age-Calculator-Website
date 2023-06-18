6393c78c04a35ef0bd82fa1c15f8adbc5a934979
smit_valture <valture16@gmail.com>
2023-06-18 13:44:29 +0530

    error text size fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -128,27 +128,27 @@ const Home = () => {
 
                 <label htmlFor="day" className={`${error.day ? "text-red-500" : "text-gray-500"} text-xs md:text-sm font-semibold tracking-[4px]`}>DAY</label>
                 <input onChange={handleDayInputChange} className={`${error.day ? "text-red-500 border-red-300" : "border-gray-300 hover:border-purple-400"} px-3 md:px-5 py-2 md:py-3 rounded-md border-2 focus:outline-2 outline-[#7d50f5] text-lg md:text-2xl font-extrabold placeholder:font-extrabold`} type="text" value={daysInput} minLength={1} maxLength={2} name="day" id="day" placeholder='DD' />
-                {error.day && <p className='text-red-400 text-xs italic'>{error.day}</p>}
+                {error.day && <p className='text-red-400 text-[11px] italic'>{error.day}</p>}
 
               </div>
               <div className='flex flex-col gap-1 w-[35%] md:w-[30%]'>
 
                 <label htmlFor="month" className={`${error.month ? "text-red-400" : "text-gray-500"} text-xs md:text-sm font-semibold tracking-[4px]`}>MONTH</label>
                 <input ref={monthInputRef} onChange={handleMonthInputChange} className={`${error.month ? "text-red-500 border-red-300" : "border-gray-300 hover:border-purple-400"} px-3 md:px-5 py-2 md:py-3 rounded-md border-2 focus:outline-2 outline-[#7d50f5] text-lg md:text-2xl font-extrabold placeholder:font-extrabold`} type="text" value={monthsInput} minLength={1} maxLength={2} name="month" id="month" placeholder='MM' />
-                {error.month && <p className='text-red-400 text-xs italic'>{error.month}</p>}
+                {error.month && <p className='text-red-400 text-[11px] italic'>{error.month}</p>}
 
               </div>
               <div className='flex flex-col gap-1 w-[35%] md:w-[30%]'>
 
                 <label htmlFor="year" className={`${error.year ? "text-red-400" : "text-gray-500"} text-xs md:text-sm font-semibold tracking-[4px]`}>YEAR</label>
                 <input ref={yearInputRef} onChange={(e) => { setYearsInput(e.target.value); setError({ ...error, year: "" }) }} className={`${error.year ? "text-red-500 border-red-300" : "border-gray-300 hover:border-purple-400"} px-3 md:px-5 py-2 md:py-3 rounded-md border-2 focus:outline-2 outline-[#7d50f5] text-lg md:text-2xl font-extrabold placeholder:font-extrabold`} type="text" value={yearsInput} minLength={4} maxLength={4} name="year" id="year" placeholder='YYYY' />
-                {error.year && <p className='text-red-400 text-xs italic'>{error.year}</p>}
+                {error.year && <p className='text-red-400 text-[11px] italic'>{error.year}</p>}
 
               </div>
             </div>
 
             <div className='relative flex justify-center w-full h-[2px] bg-gray-300'>
-              <button onClick={handleErrors} type="submit" disabled={error.day || error.month || error.year} className='absolute md:right-0 -top-7 md:-top-10 w-14 h-14 md:w-20 md:h-20 p-3 md:p-4 duration-300 bg-[#7d50f5] hover:bg-black rounded-full hover:shadow-inner shadow-white disabled:bg-gray-500'>
+              <button onClick={handleErrors} type="submit" disabled={error.day || error.month || error.year} className='absolute md:right-0 -top-7 md:-top-10 w-14 h-14 md:w-20 md:h-20 p-3 md:p-4 duration-300 bg-[#7d50f5] hover:bg-black rounded-full hover:shadow-inner shadow-white disabled:bg-gray-500 disabled:cursor-not-allowed'>
                 <img src={iconButton} alt="buttonIcon" />
               </button>
             </div>
